40f286841b2feee99b3bf3e6bf1fbcbe741060d7
Ivan Kurnia <ivan.kurnia.s@gmail.com>
2020-06-12 17:15:50 +0700

    Tabel tidak perlu dihapus

--- php_sql/database_sql/05_m_user_registration.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `u216319883_beras_m_db`.`user_registration`;
-CREATE TABLE `u216319883_beras_m_db`.`user_registration`
-(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
